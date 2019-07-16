09293ea31c4459f31f9e07f342c20793088a7b24
albert18 <caps18@hotmail.com>
2019-07-15 22:41:30 -0400

    Update

---
+++ Section2/Calculator/Calculator.Test/CalculatorTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.Library.Tests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        [TestCategory("Calculator")]
+        [TestProperty("Test Group", "Security")]
+        [Priority(1)]
+        public void Divide_PositiveNumbers_ReturnsPositiveQuotient()
+        {
+            // Arrange
+            int expected = 5;
+            int numerator = 20;
+            int denominator = 4;
+
+            // Act
+            int actual = Calculator.Divide(numerator, denominator);
+            System.Threading.Thread.Sleep(1000);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [TestCategory("Calculator")]
+        [TestProperty("Test Group", "Security")]
+        [Owner("Albert")]
+        [Priority(1)]
+        public void Divide_PositiveNumeratorAndNegativeDenominator_ReturnsNegativeQuotient()
+        {
+            // Arrange
+            int expected = -5;
+            int numerator = 20;
+            int denominator = -4;
+
+            // Act
+            int actual = Calculator.Divide(numerator, denominator);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [TestCategory("Calculator")]
+        [TestProperty("Test Group", "Performance")]
+        [Owner("Albert")]
+        [Priority(1)]
+        public void Divide_NegativeNumbers_ReturnsPositiveQuotient()
+        {
+            // Arrange
+            int expected = 5;
+            int numerator = -20;
+            int denominator = -4;
+
+            // Act
+            int actual = Calculator.Divide(numerator, denominator);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Section2/Calculator/Calculator.Test/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [TestCategory("Demo"), TestCategory("Calculator")]
+        [TestProperty("Test Group", "Performance")]
+        [Owner("Techonolgy")]
+        [Priority(2)]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod]
+        [TestCategory("Demo")]
+        [TestProperty("Test Group", "Security")]
+        [Owner("Techonolgy")]
+        [Priority(2)]
+        public void TestMethod2()
+        {
+        }
+    }
+}
