7c868c5a0db1af3b60416e3c0c00c667690bf6ab
albert18 <caps18@hotmail.com>
2019-07-16 21:11:13 -0400

    update

--- Section2/Calculator/Calculator.Test/UnitTest1.cs
+++ Section2/Calculator/Calculator.Test/UnitTest1.cs
@@ -6,6 +6,21 @@ namespace Calculator.Test
     [TestClass]
     public class UnitTest1
     {
+        private TestContext _testContext;
+
+        public TestContext TestContext
+        {
+            get
+            {
+                return _testContext;
+            }
+            set
+            {
+                _testContext = value;
+            }
+        }
+
+
         [TestMethod]
         [TestCategory("Demo"), TestCategory("Calculator")]
         [TestProperty("Test Group", "Performance")]
@@ -22,6 +37,8 @@ namespace Calculator.Test
         [Priority(2)]
         public void TestMethod2()
         {
+            System.Diagnostics.Debug.WriteLine("Debug : TM2 executed");
+            TestContext.WriteLine("TestContext : TM2 executed");
         }
     }
 }
