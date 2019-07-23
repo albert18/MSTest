da08b945edd2497c8db078de3dfde73143f7e35a
albert18 <caps18@hotmail.com>
2019-07-23 15:01:31 -0400

    update

---
+++ Section4/Calculator/Calculator/Calculator.Library/Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Calculator.Library
+{
+    public class Calculator
+    {
+        public static int Divide(int numerator, int denominator)
+        {
+            if (denominator == 0)
+                throw new DivideByZeroException("Denominator cannot be ZERO");
+            
+            int result = numerator / denominator;
+            return result;
+        }
+
+        public int Add(int fn, int sn)
+        {
+            if (IsPositive(fn) && IsPositive(sn))
+            {
+                int result = fn + sn;
+                return result;
+            }
+            else
+            {
+                throw new ArgumentException("Only positive numbers are allwoed");
+            }
+        }
+
+        private bool IsPositive(int Number)
+        {
+            return Number > 0;
+        }
+    }
+}
---
+++ Section4/Calculator/Calculator/Calculator.Test/CalculatorTests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator.Library;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Calculator.Library.Tests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        [TestCategory("Calculator")]
+        [TestProperty("Test Group", "Security")]
+        [Priority(1)]
+        public void Divide_PositiveNumbers_ReturnsPositiveQuotient_1()
+        {
+            // Arrange
+            int expected = 5;
+            int numerator = 20;
+            int denominator = 4;
+
+            // Act
+            int actual = Calculator.Divide(numerator, denominator);
+            System.Threading.Thread.Sleep(1000);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [TestCategory("Calculator")]
+        [TestProperty("Test Group", "Security")]
+        [Owner("Venkat")]
+        [Priority(1)]
+        public void Divide_PositiveNumeratorAndNegativeDenominator_ReturnsNegativeQuotient_2()
+        {
+            // Arrange
+            int expected = -5;
+            int numerator = 20;
+            int denominator = -4;
+
+            // Act
+            int actual = Calculator.Divide(numerator, denominator);
+            System.Threading.Thread.Sleep(400);
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [TestCategory("Calculator")]
+        [TestProperty("Test Group", "Performance")]
+        [Owner("Venkat")]
+        [Priority(1)]
+        public void Divide_NegativeNumbers_ReturnsPositiveQuotient_3()
+        {
+            // Arrange
+            int expected = -5;
+            int numerator = -20;
+            int denominator = -4;
+
+            // Act
+            int actual = Calculator.Divide(numerator, denominator);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void Divide_DenominatorIsZero_ThorwsDivideByZeroException_4()
+        {
+            // Arrange
+            int numerator = 20;
+            int denominator = 0;
+
+            // Act
+            try
+            {
+                Calculator.Divide(numerator, denominator);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Assert.AreEqual("Denominator cannot be ZERO", ex.Message);
+                throw;
+            }
+        }
+
+        [TestMethod]
+        public void IsPositive_PositiveNumber_ReturnsTrue_5()
+        {
+            PrivateObject p = new PrivateObject(typeof(Calculator));
+            bool actual = (bool)p.Invoke("IsPositive", -10);
+
+            Assert.IsFalse(actual);
+        }
+    }
+}
